6e015a57134caea435ee86fffc161f77957d1a38
cacaciw <tarissa.mutia@gmail.com>
2024-09-17 16:25:32 +0700

    Trying to debug

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)@dh4h^u@m_jx0=3^hi6i0ycjbut9897-&h_&j!rqm5y0b57_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tarissa-mutia-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
